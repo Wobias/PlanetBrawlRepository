6ae52ff4b584f29e1e7e9944a6306cdf73755142
Tobias Wittmann <T_Wittmann@hotmail.com>
2018-09-12 14:18:15 +0200

    update

---
+++ PlanetBrawl/Assets/Scripts/AlternativePlayerMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlternativePlayerMove : MonoBehaviour
+{
+    public float speed = 10f;
+
+    Vector2 direction;
+    Rigidbody2D myRigidbody;
+
+    // Use this for initialization
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction.x = Input.GetAxis("Horizontal");
+        direction.y = Input.GetAxis("Vertical");
+        myRigidbody.AddForce(direction * speed * Time.deltaTime);
+
+     
+    }
+}
