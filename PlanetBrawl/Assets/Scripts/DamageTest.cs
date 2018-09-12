9403c4292c310b905f4d53e2145f53b836036e4d
Tobias Wittmann <T_Wittmann@hotmail.com>
2018-09-12 17:02:11 +0200

    Damage Interface

---
+++ PlanetBrawl/Assets/Scripts/DamageTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageTest : MonoBehaviour
+{
+    public float dmg = 10f;
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.GetComponent<IDamageable>() != null)
+        {
+            other.GetComponent<IDamageable>().Hit(dmg);
+        }
+
+    }
+}
---
+++ PlanetBrawl/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour,IDamageable
+{
+    public float health = 100f;
+
+    Transform myTransform;
+    Vector3 myVector3;
+
+    // Use this for initialization
+    void Start()
+    {
+        myTransform = GetComponent<Transform>();
+        myVector3 = myTransform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health < 1)
+        {
+            Die();
+        }
+
+        
+
+    }
+
+    public void Hit(float damage)
+    {
+        if (health >= 1)
+        {
+            health -= damage;
+
+            DownScaling();
+
+            Debug.Log(health);
+        }
+    }
+
+    public void Die()
+    {
+        Debug.Log("player dead");
+    }
+
+    void DownScaling()
+    {
+        myVector3 *= 0.5f;
+        myTransform.localScale = myVector3;
+    }
+
+}
