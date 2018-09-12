e5af557e6e5503b505deae6ec06ba8c7e8036cbe
DasNanda <Nandix.myroom@gmx.at>
2018-09-12 16:57:18 +0200

    Moon Punch Update

--- PlanetBrawl/Assets/Scripts/WeaponController_Moon.cs
+++ PlanetBrawl/Assets/Scripts/WeaponController_Moon.cs
@@ -28,9 +28,9 @@ public class WeaponController_Moon : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        if (Input.GetAxis("AimHor") != 0 || Input.GetAxis("AimVer") != 0)
         {
-            myVector2 = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            myVector2 = new Vector2(Input.GetAxis("AimHor"), Input.GetAxis("AimVer"));
 
             //Calculate an angle from the direction vector
             float angle = Mathf.Atan2(myVector2.y, myVector2.x) * Mathf.Rad2Deg;
@@ -42,6 +42,7 @@ public class WeaponController_Moon : MonoBehaviour
         {
             moonState = MoonState.shooting;
             canShoot = false;
+            rb2d.isKinematic = false;
         }
         else if (Input.GetButtonUp("Fire1"))
         {
@@ -50,9 +51,9 @@ public class WeaponController_Moon : MonoBehaviour
 
         if (moonState == MoonState.shooting)
         {
-            if (moonPos.localPosition.x < maxDistance || moonPos.localPosition.y < maxDistance)
+            if ((holderPos.position - moonPos.position).magnitude < maxDistance)
             {
-                rb2d.velocity = moonPos.right * retractSpeed * Time.deltaTime;
+                rb2d.velocity = -(holderPos.position - moonPos.position).normalized * retractSpeed * Time.deltaTime;
             }
             else
             {
@@ -61,17 +62,24 @@ public class WeaponController_Moon : MonoBehaviour
         }
         else if (moonState == MoonState.retracting)
         {
-            if (moonPos.localPosition.x > minDistance || moonPos.localPosition.y > minDistance)
+            if ((holderPos.position - moonPos.position).magnitude > minDistance)
             {
-                rb2d.velocity = -moonPos.right * retractSpeed * Time.deltaTime;
+                rb2d.velocity = (holderPos.position - moonPos.position).normalized * retractSpeed * Time.deltaTime;
             }
             else
             {
                 rb2d.velocity = Vector2.zero;
-                moonPos.position = moonPos.right * minDistance;
+                moonPos.position = holderPos.position + minDistance * moonPos.right;
                 moonState = MoonState.orbit;
                 canShoot = true;
+                rb2d.isKinematic = true;
             }
         }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(holderPos.position, minDistance);
     }
 }
