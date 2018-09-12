943924c3d03fda5b6c6e49e035be1ccb97c2ef8a
Reload322 <michael.mutsch1@gmx.at>
2018-09-13 01:26:51 +0200

    Edited DynamicEnvironment.cs to AsteroidSpawner.cs and Added AsteroidMovement.cs

---
+++ PlanetBrawl/Assets/Scripts/AsteroidMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidMovement : MonoBehaviour {
+    private Rigidbody2D rb;
+    private Transform myTransform;
+    public float rotateSpeed = 50f;
+    public float movementSpeed = 10f;
+
+
+    // Use this for initialization
+    void Start () {
+        myTransform = gameObject.transform;
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        rb.AddForce(new Vector2(movementSpeed, movementSpeed));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        myTransform.Rotate(Vector3.back * Time.deltaTime * rotateSpeed);
+        
+    }
+}
---
+++ PlanetBrawl/Assets/Scripts/AsteroidSpawner.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidSpawner : MonoBehaviour
+{
+
+   
+    private float spawnTimer = 5;
+    public int spawnMin = 0;
+    public int spawnMax = 1000;
+    public GameObject asteroid;
+    
+    public float minX = -3.7f;
+    public float maxX = 3.7f;
+    public float minY = -7.4f;
+    public float maxY = 7.4f;
+
+   
+    void Update()
+    {
+        /* Spawn asteroid with a random chance */
+        //float sum = Random.Range(spawnMin, spawnMax);
+        //Debug.Log(sum);
+        //if (sum == 50)
+        //{
+        //    Invoke("SpawnMeteor", 1);
+
+        //}
+
+        /*Spawn asteroid after Time*/
+        spawnTimer -= Time.deltaTime;
+        Debug.Log(spawnTimer);
+        if (spawnTimer < 0)
+        {
+            
+            spawnTimer = 5;
+        }
+
+        if (spawnTimer == 5)
+        {
+            Invoke("SpawnMeteor", 1);
+            
+
+
+        }
+        
+        
+        
+
+    }
+    void SpawnMeteor()
+    {
+        float posX = Random.Range(minX, maxX);
+        float posY = Random.Range(minY, maxY);
+        Vector2 spawnPosition = new Vector2(posX, posY);
+        Instantiate(asteroid, spawnPosition, Quaternion.identity);
+        
+    }
+}
+
