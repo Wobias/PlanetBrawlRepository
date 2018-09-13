5d51c6c15fa3d0e1f8fb9058dd3be2e1ba4a0bca
DasNanda <Nandix.myroom@gmx.at>
2018-09-13 17:08:12 +0200

    Camera System & Parallax

---
+++ PlanetBrawl/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float followSpeed;
+    public float zoomSpeed;
+
+    private Transform[] players = new Transform[4];
+    private Transform cam;
+    private Vector3 target;
+
+    private Vector3 minBounds;
+    private Vector3 maxBounds;
+
+
+    void Start()
+    {
+        cam = transform;
+        target = cam.position;
+
+        GameObject[] playerObjects = GameObject.FindGameObjectsWithTag("Player");
+
+        players = new Transform[playerObjects.Length];
+
+        for (int i = 0; i < playerObjects.Length; i++)
+        {
+            players[i] = playerObjects[i].transform;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (players != null)
+        {
+            minBounds = players[0].position;
+            maxBounds = players[0].position;
+
+            for (int i = 1; i < players.Length; i++)
+            {
+                if (players[i].position.x < minBounds.x)
+                {
+                    minBounds.x = players[i].position.x;
+                }
+                else if (players[i].position.x > maxBounds.x)
+                {
+                    maxBounds.x = players[i].position.x;
+                }
+
+                if (players[i].position.y < minBounds.y)
+                {
+                    minBounds.y = players[i].position.y;
+                }
+                else if (players[i].position.y > maxBounds.y)
+                {
+                    maxBounds.y = players[i].position.y;
+                }
+            }
+
+            target = minBounds + (maxBounds - minBounds) / 2 + new Vector3(0,0,cam.position.z);
+        }
+        
+        cam.position = target;
+    }
+}
---
+++ PlanetBrawl/Assets/Scripts/Parallaxer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallaxer : MonoBehaviour
+{
+    public float parallaxSpeed;
+
+    private Material backgroundMaterial;
+    private Vector2 startOffset;
+    private Vector2 offset;
+
+    void Start()
+    {
+        backgroundMaterial = GetComponent<MeshRenderer>().material;
+
+        startOffset = backgroundMaterial.mainTextureOffset;
+    }
+
+    void LateUpdate()
+    {
+        offset = new Vector2(transform.position.x * 0.01f * parallaxSpeed, transform.position.y * 0.01f * parallaxSpeed);
+        backgroundMaterial.mainTextureOffset = offset + startOffset;
+    }
+}
