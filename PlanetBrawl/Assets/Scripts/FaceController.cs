da52e693c2d264f739f83961b6263024cd44d1d6
DasNanda <Nandix.myroom@gmx.at>
2018-09-18 21:26:49 +0200

    -> Added Face Movement

---
+++ PlanetBrawl/Assets/Scripts/FaceController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceController : MonoBehaviour
+{
+    public int player = 1;
+    public float maxDistance = 0.1f;
+    public float lookSpeed = 0.25f;
+
+    private Vector2 direction;
+    private Transform face;
+
+
+    void Start()
+    {
+        face = transform;
+    }
+
+    void Update ()
+    {
+        //AIMING
+        #region
+
+        //Get the Aiming Direction
+        direction = new Vector2(Input.GetAxis("AimHor" + player), Input.GetAxis("AimVer" + player));
+
+        if (direction == Vector2.zero)
+        {
+            direction = new Vector2(Input.GetAxis("Horizontal" + player), Input.GetAxis("Vertical" + player));
+        }
+
+        face.localPosition = Vector2.Lerp(face.localPosition, direction * maxDistance, lookSpeed);
+
+        #endregion
+    }
+}
