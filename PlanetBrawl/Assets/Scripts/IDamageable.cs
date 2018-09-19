a0bc2cbd61895197bbdc91cb8528ae7998468af7
DasNanda <Nandix.myroom@gmx.at>
2018-09-19 23:30:47 +0200

    -> Added Item Pickups

--- PlanetBrawl/Assets/Scripts/IDamageable.cs
+++ PlanetBrawl/Assets/Scripts/IDamageable.cs
@@ -5,6 +5,5 @@ using UnityEngine;
 public interface IDamageable
 {
     void Hit(float damage);
-
 }
 
---
+++ PlanetBrawl/Assets/Scripts/ItemPickup.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+    public GameObject itemPrefab;
+
+    public enum ItemType { weapon, powerUp };
+    public ItemType itemType;
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        WeaponController weaponController = other.GetComponent<WeaponController>();
+
+        if (weaponController == null)
+            return;
+
+        GameObject item = Instantiate(itemPrefab, other.transform.position, Quaternion.identity, other.transform);
+
+        if (itemType == ItemType.weapon)
+        {
+            weaponController.WeaponPickUp(item);
+        }
+
+        Destroy(gameObject);
+    }
+}
