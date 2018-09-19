c5f2706e1746c01f9362bae22175ab2cfc08f140
sebnes <41290900+sebnes@users.noreply.github.com>
2018-09-19 21:08:50 +0200

    ItemSpawn

---
+++ PlanetBrawl/Assets/Scripts/ItemSpawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawn : MonoBehaviour
+{
+    public GameObject item;
+	
+	void Update ()
+    {
+		
+	}
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        GameObject otherObject = coll.gameObject;
+
+        if (otherObject.tag == "Asteroid" /*"Tag"*/)
+        {
+            float chance = Random.Range(0f, 10f);
+            if (chance <= 5)
+            {
+                Instantiate(/* Item */item, otherObject.transform.position, Quaternion.identity);
+            }
+        }
+            
+    }
+}
