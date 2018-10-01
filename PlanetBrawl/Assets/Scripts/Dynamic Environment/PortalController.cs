16eba032e648d3aea59feb15d5f2d314eab93142
Reload322 <michael.mutsch1@gmx.at>
2018-10-02 00:41:11 +0200

    ->Added PortalController.cs

---
+++ PlanetBrawl/Assets/Scripts/Dynamic Environment/PortalController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalController : MonoBehaviour
+{
+    public GameObject otherPortal;
+    public bool isPortalReady = true;
+    public float coolDown = 3f;
+
+
+    void Update()
+    {
+        if (isPortalReady == false)
+        {
+            coolDown -= Time.deltaTime;
+            if (coolDown <= 0)
+            {
+                isPortalReady = true;
+                coolDown = 3f;
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player" && isPortalReady == true)
+        {
+            other.gameObject.transform.position = new Vector2(otherPortal.transform.position.x, otherPortal.transform.position.y);
+            isPortalReady = false;
+            otherPortal.GetComponent<PortalController>().isPortalReady = false;
+        }
+    }
+}
