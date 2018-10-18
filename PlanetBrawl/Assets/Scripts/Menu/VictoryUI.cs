9b27db43697277f1e0f623eb5d0f96e6d58438fe
Tobias Wittmann <T_Wittmann@hotmail.com>
2018-10-18 18:27:21 +0200

    bla

---
+++ PlanetBrawl/Assets/Scripts/Menu/VictoryUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VictoryUI : MonoBehaviour
+{
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void StartFreeForAll()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void StartTeamFight()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
