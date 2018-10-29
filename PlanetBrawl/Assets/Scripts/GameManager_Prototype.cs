24f1234b3cf2540639689accca9e2f2ca217719a
Tobias Wittmann <T_Wittmann@hotmail.com>
2018-10-29 13:36:26 +0100

    improved victory screen

--- PlanetBrawl/Assets/Scripts/GameManager_Prototype.cs
+++ PlanetBrawl/Assets/Scripts/GameManager_Prototype.cs
@@ -10,12 +10,12 @@ public class GameManager_Prototype : MonoBehaviour
     public static GameManager_Prototype gameManager;
 
     public List<GameObject> players = new List<GameObject>();
-
     public List<GameObject> teamOne = new List<GameObject>();
     public List<GameObject> teamTwo = new List<GameObject>();
 
-
     public GameObject victoryScreen;
+
+    public TextMeshProUGUI victoryText;
 
     public bool teamMode;
     public bool gameOver;
@@ -99,7 +99,6 @@ public class GameManager_Prototype : MonoBehaviour
             SetLayer(child, layer);
     }
 
-
     public void RemoveFromLists(GameObject deadPlayer)
     {
         if (deadPlayer != null)
@@ -120,7 +119,6 @@ public class GameManager_Prototype : MonoBehaviour
         }
     }
 
-
     public void VictoryConditions()
     {
         if (teamMode)
@@ -128,12 +126,30 @@ public class GameManager_Prototype : MonoBehaviour
             if (teamOne.Count <= 0)
             {
                 Debug.Log("Team Two is victorious");
+                victoryText.SetText ("Team Two Victory!");
                 victoryScreen.SetActive(true);
+
+                if (teamTwo.Count > 1)
+                {
+                    teamTwo[0].transform.position = new Vector3(-1.5f, 0f, 0f);
+                    teamTwo[1].transform.position = new Vector3(1.5f, 0f, 0f);
+                    teamTwo[0].isStatic = true;
+                    teamTwo[1].isStatic = true;
+                }
             }
-            else if (teamTwo.Count <= 0)
+            if (teamTwo.Count <= 0)
             {
                 Debug.Log("Team One is victorious");
+                victoryText.SetText("Team One Victory!");
                 victoryScreen.SetActive(true);
+
+                if (teamOne.Count > 1)
+                {
+                    teamOne[0].transform.position = new Vector3(-1f, 0f, 0f);
+                    teamOne[1].transform.position = new Vector3(1f, 0f, 0f);
+                    teamOne[0].isStatic = true;
+                    teamOne[1].isStatic = true;
+                }
             }
         }
         else if (!teamMode)
@@ -142,8 +158,11 @@ public class GameManager_Prototype : MonoBehaviour
             {
                 if (players.Count == 1)
                 {
-                    Debug.Log(players[0] + " " + " is victorious");
+                    Debug.Log(players[0].name + " is victorious");
+                    victoryText.SetText(players[0].name + " Victory!");
                     victoryScreen.SetActive(true);
+                    players[0].transform.position = new Vector3(0f, 0f, 0f);
+                    players[0].isStatic = true;
                 }
             }
         }
