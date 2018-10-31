8c3dce0eba2078e1bbbf10f070bf370f9cd7e734
Tobias Wittmann <T_Wittmann@hotmail.com>
2018-10-31 16:40:59 +0100

    Portals

--- PlanetBrawl/Assets/Scripts/Dynamic Environment/PortalController.cs
+++ PlanetBrawl/Assets/Scripts/Dynamic Environment/PortalController.cs
@@ -5,30 +5,19 @@ using UnityEngine;
 public class PortalController : MonoBehaviour
 {
     public GameObject otherPortal;
-    public bool isPortalReady = true;
-    public float coolDown = 3f;
-
-
-    void Update()
-    {
-        if (isPortalReady == false)
-        {
-            coolDown -= Time.deltaTime;
-            if (coolDown <= 0)
-            {
-                isPortalReady = true;
-                coolDown = 3f;
-            }
-        }
-    }
-
+    
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.tag == "Player" && isPortalReady == true)
+        if (other.gameObject.tag == "Player")
         {
             other.gameObject.transform.position = new Vector2(otherPortal.transform.position.x, otherPortal.transform.position.y);
-            isPortalReady = false;
-            otherPortal.GetComponent<PortalController>().isPortalReady = false;
+
+            PortalSpawner.portalSpawner.portalIsOpen = false;
+            PortalSpawner.portalSpawner.portalTimer = Random.Range(2f, 8f);
+
+            otherPortal.SetActive(false);
+            gameObject.SetActive(false);
         }
     }
+
 }
---
+++ PlanetBrawl/Assets/Scripts/Dynamic Environment/PortalSpawner.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalSpawner : MonoBehaviour
+{
+    public static PortalSpawner portalSpawner;
+
+    public GameObject portalOne;
+    public GameObject portalTwo;
+
+    public float portalTimer = 5f;
+    private float portalResetTimer = 10f;
+
+    public bool portalIsOpen = false;
+
+    private Vector2 portalOnePosition;
+
+    private void Start()
+    {
+        portalSpawner = this;
+
+        portalOne.SetActive(false);
+        portalTwo.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        portalTimer -= Time.deltaTime;
+
+        if (portalTimer <= 0f && portalIsOpen == false)
+        {
+            SetPortal();
+            portalTimer = Random.Range(3f, 10f);
+        }
+
+        if (portalIsOpen == true)
+        {
+            portalResetTimer -= Time.deltaTime;
+
+            if (portalResetTimer <= 0f)
+            {
+                portalIsOpen = false;
+                portalResetTimer = Random.Range(5f, 15f);
+            }
+        }
+    }
+
+    private void SetPortal()
+    {
+        portalOnePosition.x = Random.Range(-18f, 19f);
+        portalOnePosition.y = Random.Range(-8f, 9f);
+
+        portalOne.transform.position = portalOnePosition;
+        portalTwo.transform.position = portalOnePosition * -1;
+
+        portalOne.SetActive(true);
+        portalTwo.SetActive(true);
+
+        portalIsOpen = true;
+    }
+}
